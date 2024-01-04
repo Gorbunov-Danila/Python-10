d43ff050a826415b42101190b764c6498981a421
Gorbunov-Danila <gorbunovdanil345@gmail.com>
2024-01-04 18:07:30 +0300

    1

---
+++ individ.py
@@ -0,0 +1,17 @@
+
+if __name__ == "__main__":
+   u = set("abcdefghijklmnopqrstuvwxyz")
+   
+   a = {"a", "f", "I", "n", "o"}
+   b = {"f", "g", "o", "p", "z"}
+   c = {"i", "j", "u", "w"}
+   d = {"f", "h", "n", "t", "u", "y", "z"}
+   
+   x = (a.intersection(b)).union(c)
+   print(f"x = {x}")
+   
+   an = u.difference(a)
+   bn = u.difference(b)
+   
+   y = (an.intersection(bn)).difference(c.union(d))
+   print(f"y = {y}")
---
+++ zadacha 2.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+    text1 = input("Введите первое предложение: ")
+    text2 = input("Введите второе предложение: ")
+
+    plenty1 = set(text1)
+    plenty2 = set(text2)
+
+    shared_sym = plenty1.intersection(plenty2)
+
+    print("Общие символы:", shared_sym)
